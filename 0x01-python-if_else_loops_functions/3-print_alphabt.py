88a338815f6ca1c7a7c9fa771d1ba858f1710adb
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-05 21:37:08 +0300

    Ascii except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
     if chr(char) is not 'q' and chr(char) is not 'e':
-        print("{}".format(chr(char)), end="")
+        print("{:c}".format(char), end="")
