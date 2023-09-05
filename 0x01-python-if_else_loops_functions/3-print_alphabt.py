174676ef2854b0c56e505fd8af80ee16cf971cd7
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-05 21:42:14 +0300

    ascii

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
-    if chr(char) is not 'q' and chr(char) is not 'e':
-        print("{}".format(char), end="")
+    if chr(char) != 'q' and chr(char) != 'e':
+        print("{}".format(chr(char)), end="")
