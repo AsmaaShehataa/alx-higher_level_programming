95f9bf60b947a57e4edd88044566daa675c6c9c4
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-07 20:22:13 +0300

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add  # Import the 'add' function from 'add_0'
+    from add_0 import add
 
     a = 1
     b = 2
-    result = add(a, b)  # Call the 'add' function
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
