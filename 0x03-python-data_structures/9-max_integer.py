4091bcc780d42ea3c26de7efe1bd0fa38e6b632c
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-09 10:16:42 +0300

    max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,10 @@
 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
-    else:
-        return max(my_list)
+        return (None)
+    mylen_str = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > mylen_str:
+            mylen_str = my_list[i]
+
+    return (mylen_str)
