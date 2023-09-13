8e24b6185e4d998bfc6136a326692837ca05145b
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-14 01:18:25 +0300

    Romanian

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
 
-def to_subt(list_num):
+def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
 
-    for i in list_num:
-        if max_list > i:
-            to_sub += i
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
     return (max_list - to_sub)
 
 
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+
     if not isinstance(roman_string, str):
         return 0
 
-    romanin = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(romanin)
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
 
     num = 0
     last_rom = 0
     list_num = [0]
 
     for ch in roman_string:
-        for num in list_keys:
-            if num == ch:
-                if romanin.get(ch) <= last_rom:
-                    num += to_subt(list_num)
-                    list_num = [romanin.get(ch)]
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
                 else:
-                    list_num.append(romanin.get(ch))
-                last_rom = romanin.get(ch)
-        num += to_subt(list_num)
-        return (num)
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
