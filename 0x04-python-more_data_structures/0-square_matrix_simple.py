88afe67712d6b4754e2bb9cb609c0344f1c676ca
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-13 09:51:55 +0300

    edit pycode style

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 
 def square_matrix_simple(matrix=[]):
     copied_matrix = matrix.copy()
-    
+
     for i in range(len(matrix)):
         copied_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
