fc3542bcc7181dd2fffd43d4fba1573fca7f5ffb
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-13 09:33:21 +0300

    DS - Sqrt root

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+
+def square_matrix_simple(matrix=[]):
+    copied_matrix = matrix.copy()
+    
+    for i in range(len(matrix)):
+        copied_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (copied_matrix)
