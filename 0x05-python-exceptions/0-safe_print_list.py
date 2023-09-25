bb24cb09a06cd7813e62389fa8f35ccd12c22f96
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-26 00:23:31 +0300

    Task-0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    my_index = 0
+
+    while my_index < x:
+        try:
+            print("{}".format(my_list[my_index]), end="")
+            my_index += 1
+        except IndexError:
+            break
+
+    print()
+    return(my_index)
