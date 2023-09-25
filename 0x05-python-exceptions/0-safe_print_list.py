9f427fa773fa1ef3afff324565748b0e7f87fb18
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-26 00:36:45 +0300

    0-safe pritn lsit file edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+
     my_index = 0
 
     while my_index < x:
         try:
             print("{}".format(my_list[my_index]), end="")
             my_index += 1
-        except IndexError:
-            break
+        except (ValueError, TypeError):
+            continue
 
     print()
     return(my_index)
