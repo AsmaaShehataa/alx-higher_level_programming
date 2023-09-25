1e3dd0ae09c89ed9e43be3e699c68c474c8db8ba
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-26 00:39:54 +0300

    0-safe print list last version

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[my_index]), end="")
             my_index += 1
-        except (ValueError, TypeError):
-            continue
+        except IndexError:
+            break
 
     print()
     return(my_index)
