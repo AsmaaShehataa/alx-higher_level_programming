2ca327fedebd35e3514c019b19569d1893bada88
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-09-27 00:46:16 +0300

    1-Squre

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""file defines a sqre"""
+
+class Square:
+    """a class sqre that defines a square"""
+    def __init__(self, size=None):
+        """creatinf a private ATT size"""
+        self.__size = size
