e668ab6dfc70f7cfa04c54081155a4fa84c95e37
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-10-09 13:32:58 -0700

    python code style

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-
 """
-contains MyList class 
+contains the MyList class
 """
-
 
 
 class MyList(list):
-    """a subclass of the list"""
+    """a subclass of list"""
     def __init__(self):
-        """object initializing"""
+        """initializes the object"""
         super().__init__()
 
-
-
     def print_sorted(self):
-        """print the sorted list"""
+        """prints the sorted list"""
         print(sorted(self))
