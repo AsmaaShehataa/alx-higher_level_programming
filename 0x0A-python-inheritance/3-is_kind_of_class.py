2fe56d935a5f6c037c68673cf0d8509ed227f525
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-10-09 14:23:44 -0700

    a is a kind of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Module contains is_kind_of_class method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return True if the object is an instance otherwise return Fals"""
+    return isinstance(obj, a_class)
