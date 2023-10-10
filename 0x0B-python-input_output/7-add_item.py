ab0dd859f1994e1740c91048f06af4629fab9e7b
Asmaa Shehata <asmaa_shihata@yahoo.com>
2023-10-10 10:08:01 -0700

    saving file in a json format

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""
- save items to a file.
-"""
+""" script that adds all arguments to a Python list"""
 
 import sys
-import argv
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+args = list(sys.argv[1:])
+
 try:
-    try:
-        json_list = load_file('add_item.json')
-    except (ValueError, FileNotFoundError):
-        json_list = []
+    O-data = load_from_json_file('add_item.json')
+except Exception:
+    O-data = []
 
-for item in argv[1:]:
-    json_list.append(item)
-
-save_file(json_list, 'add_item.json')
+O-data.extend(arglist)
+save_to_json_file(O-data, 'add_item.json')
