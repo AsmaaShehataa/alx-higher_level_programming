d8544b62d01b9a0b8559190f0d3ff642eac56909
Replit user <>
2023-10-10 13:56:52 +0000

    python code style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Reading text file"""
 
-
 def read_file(filename=""):
-  """Print the content of the file"""
-  with read_file(filename, encoding='utf-8') as f:
-    print(f.read(), end="")
+    """Print the content of the file"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
