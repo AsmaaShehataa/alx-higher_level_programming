f0c810f722323f1f8abb49a187e358ec78f98b9c
Replit user <>
2023-10-10 13:51:45 +0000

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-	"""Print the content of the file"""
-	with read_file("my_file_0.txt", encoding="utf-8") as file:
-		print(file.read(), end="")
+  """Print the content of the file"""
+  with read_file(filename, encoding='utf-8') as f:
+    print(f.read(), end="")
