e4567e01b0af9cff1c9a9355b747ce46c3868e2d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-10-16 19:48:42 +0300

    Pycode styling

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(Base):
         """
         Width setter
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value <= 0:
             raise ValueError('width must be > 0')
@@ -41,16 +41,16 @@ class Rectangle(Base):
     @property
     def height(self):
         """
-        height getter
+        Height getter
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        height setter
+        Height setter
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value <= 0:
             raise ValueError('height must be > 0')
@@ -59,16 +59,16 @@ class Rectangle(Base):
     @property
     def x(self):
         """
-        x getter
+        X getter
         """
         return self.__x
 
     @x.setter
     def x(self, value):
         """
-        x setter
+        X setter
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('x must be an integer')
         if value < 0:
             raise ValueError('x must be >= 0')
@@ -77,16 +77,16 @@ class Rectangle(Base):
     @property
     def y(self):
         """
-        y getter
+        Y getter
         """
         return self.__y
 
     @y.setter
     def y(self, value):
         """
-        y setter
+        Y setter
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('y must be an integer')
         if value < 0:
             raise ValueError('y must be >= 0')
@@ -103,12 +103,12 @@ class Rectangle(Base):
         Prints Rectangle to
         console with #
         """
-        for col in range(self.y):
+        for _ in range(self.y):
             print()
-        for y_axis in range(self.height):
-            for x_axis in range(self.x):
+        for _ in range(self.height):
+            for _ in range(self.x):
                 print(' ', end='')
-            for row in range(self.width):
+            for _ in range(self.width):
                 print('#', end='')
             print()
 
@@ -140,8 +140,8 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """
-       Pull the parameters out in
-       the function as a dictionary
+        Pull the parameters out in
+        the function as a dictionary
         """
         return {
             'x': self.x,
