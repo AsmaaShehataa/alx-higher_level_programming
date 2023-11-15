28f3647e29298f5211b46d7348a4a19531259095
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-11-15 19:04:13 +0200

    rivileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- a script that lists all privileges of the MySQL users
-SHOW GRANTS FOR user_0d_1@localhost
+SHOW GRANTS FOR 'user_0d_1@localhost';
 -- 2nd Query
-SHOW GRANTS FOR user_0d_2@localhost
+SHOW GRANTS FOR 'user_0d_2@localhost';
