99c63c014c49b683fed86ad409f0de3643595541
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-11-16 01:49:28 +0200

    create a database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
--- a script that Creates the database hbtn_0d_2 and the user user_0d_2.
+-- create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.*
-TO user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- create user and grant privileges
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_1@localhost;
