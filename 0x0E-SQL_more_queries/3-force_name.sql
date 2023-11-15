ffe14b0585624eea0dc71495d1688d1ae021eb76
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-11-16 01:47:28 +0200

    Create a table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
--- a script that creates the table force_name
-CREATE TABLE IF NOT EXISTS force_name(id INT, NAME VARCHAR(256) NOT NULL);
+-- create table even if not exists
+CREATE TABLE IF NOT EXISTS force_name(id INT NOT NULL, name VARCHAR(256) NOT NULL);
