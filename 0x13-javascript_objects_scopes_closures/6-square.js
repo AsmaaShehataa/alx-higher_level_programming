f454b87e1d76808887ec6989a6d0861af2a2b19d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-12-12 17:36:04 +0200

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,7 @@
+#!/usr/bin/node
+
 const Rectangle = require('./5-square');
-// Testing the Rectangle class
+
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
