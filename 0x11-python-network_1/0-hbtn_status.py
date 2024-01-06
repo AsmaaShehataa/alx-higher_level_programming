d1cd905e9e4c579a2f3884e1d9d01899a2b340ed
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-06 20:15:47 +0200

    code fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Python Script that fetches https://intranet.hbtn.io/status
+Python script that fetches https://alx-intranet.hbtn.io/status
 """
-from urllib import request
 
-myurl = 'https://intranet.hbtn.io/status'
+from urllib import request
+myurl = 'https://alx-intranet.hbtn.io/status'
 with request.urlopen(myurl) as response:
     my_page = response.read()
     print('Body response:')
-    print('\t- type: {}'.format(type(my_page_page)))
+    print('\t- type: {}'.format(type(my_page)))
     print('\t- content: {}'.format(my_page))
     print('\t- utf8 content: {}'.format(my_page.decode('utf-8')))
