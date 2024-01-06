82fee02f5a4080735a00041cf14f4d2ba4d4c43c
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-06 23:59:28 +0200

    code fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, 
+"""Python script that takes
+in a URL and an email,
 sends a POST request to the passed URL
 """
 
 if __name__ == '__main__':
-  from sys import argv
-  from urllib import request, parse
+    from sys import argv
+    from urllib import request, parse
 
-  url = argv[1]
-  email = argv[2]
+    url = argv[1]
+    email = argv[2]
 
-  my_prms = parse.urlencode({'email': email}).encode('utf-8') #encoding - sending - Post
-  with request.urlopen(url, data=my_prms) as response:
-    content = response.read().decode('utf-8') #decoding - receiving
-    print(content)
+    my_prms = parse.urlencode({'email': email}).encode('utf-8')  # encoding
+    with request.urlopen(url, data=my_prms) as response:
+        content = response.read().decode('utf-8')  # decoding
+        print(content)
