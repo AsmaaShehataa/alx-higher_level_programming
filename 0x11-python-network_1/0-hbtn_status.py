1ecc9923baad7589352192e512fbd384fba3f29e
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-06 20:12:58 +0200

    Fetching intranet website

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
+Python Script that fetches https://intranet.hbtn.io/status
 """
-
 from urllib import request
-myurl = 'https://alx-intranet.hbtn.io/status'
-with request.urlopen(myurl) as response:
-  my_page = response.read()
-  print('Body response:')
-  print('\t- type: {}'.format(type(my_page)))
-  print('\t- content: {}'.format(my_page))
-  print('\t- utf8 content: {}'.format(my_page.decode('utf-8')))
+
+url = 'https://intranet.hbtn.io/status'
+with request.urlopen(url) as response:
+    the_page = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- content: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
