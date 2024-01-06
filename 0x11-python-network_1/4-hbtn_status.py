320dd9cb88f0be2ef8fed446fc14c5d6cdd6bb4a
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-07 00:59:16 +0200

    code fix

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == '__main__':
+  import requests
+
+  url = 'https://alx-intranet.hbtn.io/status'
+
+  with requests.get(url) as r:
+        content = r.text
+        print(f"Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
