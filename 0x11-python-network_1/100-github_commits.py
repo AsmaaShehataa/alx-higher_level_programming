c5d6e437a8a8f66adca8403691297c9c0e411495
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-07 15:22:44 +0200

    10-commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Holberton School backend filteration"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    headers = {
+              'Accept': 'application/vnd.github.v3+json',
+              }
+    params = {
+        'per_page': 10,
+    }
+
+    res = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
+                      owner_name, repository_name),
+                      headers=headers, params=params)
+    json_res = res.json()
+    for commit in json_res:
+        print(commit['sha'] + ':', commit['commit']['author']['name'])
