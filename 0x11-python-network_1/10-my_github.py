f7fa354218738d942fa8952719397d7efc0f9331
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-07 15:00:22 +0200

    Handling my GitHub creds

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Python script that takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id"""
+
 import requests
 from sys import argv
-from requests.auth import HTTPBasicAuth
+
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
     username = "AsmaaShehataa"
