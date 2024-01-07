4e24593fe5d348ceef32840393c5ec4b0c11fac0
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-07 15:25:20 +0200

    code fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    repo_name = sys.argv[1]
-    owner = sys.argv[2]
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
     headers = {
               'Accept': 'application/vnd.github.v3+json',
               }
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     }
 
     res = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
-                      owner, repo_name),
+                      owner_name, repository_name),
                       headers=headers, params=params)
     json_res = res.json()
     for commit in json_res:
