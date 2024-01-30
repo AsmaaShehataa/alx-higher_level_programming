e4ef7364dd93f04565a54f5730e4a5a3158fd60d
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-30 22:24:54 +0000

    request store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fscrap = require('fscrap');
+const fs = require('fs');
 const url = process.argv[2];
 const fileName = process.argv[3];
 
-const fileStream = fscrap.createWriteStream(fileName);
+const fileStream = fs.createWriteStream(fileName);
 request(url).pipe(fileStream);
